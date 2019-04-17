ed915130f5a1a005a82baae6b29c6636e78816aa
onlyAngelia <479397375@qq.com>
2019-04-17 16:23:26 +0800

    决策树模型使用技巧总结

---
+++ 数据分析/geekTime/data/决策树泰坦尼克生存率预测.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
